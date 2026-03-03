20b7d3754dd865c3a3389ecde296e42485b3babd
Israt Jahan Jisha <isratjahanfci@gmail.com>
2026-03-03 10:19:20 +0600

    implement Custom Error handling

--- src/app/module/doctor/doctor.validate.ts
+++ src/app/module/doctor/doctor.validate.ts
@@ -1,3 +1,18 @@
 import z from "zod";
+import { gender } from "../../../generated/prisma/enums";
 
-export const updateDoctorZodSchema = z.object({}).partial();
+export const updateDoctorZodSchema = z.object({
+    doctor : z.object({
+    name: z.string("Name must be a string").min(3, "Name must be at least 3 characters long").max(20, "Name must be less than 20 characters long").optional(),
+    profilePhoto: z.url("Profile photo must be a valid URL").optional(),
+    contactNumber: z.string("Contact number must be a string").min(10, "Contact number must be at least 10 digits long").optional(),
+    address: z.string("Address must be a string").min(10, "Address must be at least 10 characters long").max(100, "Address must be less than 100 characters long").optional(),
+    experience: z.int("Experience must be an integer").nonnegative("Experience cannot be negative").optional(),
+    gender: z.enum([gender.MALE, gender.FEMALE],"Gender must be either MALE or FEMALE").optional(),
+    appointmentFee: z.number("Appointment fee must be a number").nonnegative("Appointment fee cannot be negative").optional(),
+    qualification: z.string("Qualification must be a string").min(3, "Qualification must be at least 3 characters long").max(50, "Qualification must be less than 50 characters long").optional(),
+    currentWorkingPlace: z.string("Current working place must be a string").min(3, "Current working place must be at least 3 characters long").max(50, "Current working place must be less than 50 characters long").optional(),
+    designation: z.string("Designation must be a string").min(3, "Designation must be at least 3 characters long").max(50, "Designation must be less than 50 characters long").optional(), 
+    })
+});
+// .partial();
--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -1,21 +1,83 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { Request, Response , NextFunction} from "express";
-import { envVars } from "../config/env";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextFunction, Request, Response } from "express";
 import status from "http-status";
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const globalErrorHandler = (error : any , req : Request , res : Response , next : NextFunction) =>{
-    if(envVars.NODE_ENV === "development"){
-        console.log("Error from global error handler",error)
+import {z} from "zod";
+import { envVars } from "../config/env";
+import { TErrorResponse, TErrorSources } from "../app/interfaces/error.interface";
+import { handleZodError } from "../app/errorHelpers/handleZodError";
+import AppError from "../app/errorHelpers/AppError";
+
+
+
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+    if (envVars.NODE_ENV === 'development') {
+        console.log("Error from Global Error Handler", err);
     }
 
-    const statusCode : number = status.INTERNAL_SERVER_ERROR;
-    const message : string = "Something went wrong";
+    let errorSources: TErrorSources[] = []
+    let statusCode: number = status.INTERNAL_SERVER_ERROR;
+    let message: string = 'Internal Server Error';
+    let stack: string | undefined = undefined;
+
+    //Zod Error Patttern
+    /*
+     error.issues; 
+    /* [
+      {
+        expected: 'string',
+        code: 'invalid_type',
+        path: [ 'username' , 'password' ], => username password
+        message: 'Invalid input: expected string'
+      },
+      {
+        expected: 'number',
+        code: 'invalid_type',
+        path: [ 'xp' ],
+        message: 'Invalid input: expected number'
+      }
+    ] 
+    */
+
+    if (err instanceof z.ZodError) {
+        const simplifiedError = handleZodError(err);
+        statusCode = simplifiedError.statusCode as number
+        message = simplifiedError.message
+        errorSources = [...simplifiedError.errorSources]
+        stack = err.stack;
+
+    } else if (err instanceof AppError) {
+        statusCode = err.statusCode;
+        message = err.message;
+        stack = err.stack;
+        errorSources = [
+            {
+                path: '',
+                message: err.message
+            }
+        ]
+    }
+    else if (err instanceof Error) {
+        statusCode = status.INTERNAL_SERVER_ERROR;
+        message = err.message
+        stack = err.stack;
+        errorSources = [
+            {
+                path: '',
+                message: err.message
+            }
+        ]
+    }
 
 
-    res.status(statusCode).json({
-        success : false,
-        message : message,
-        error
-    })
-    
+    const errorResponse: TErrorResponse = {
+        success: false,
+        message: message,
+        errorSources,
+        error: envVars.NODE_ENV === 'development' ? err : undefined,
+        stack: envVars.NODE_ENV === 'development' ? stack : undefined,
+    }
+
+    res.status(statusCode).json(errorResponse);
 }
