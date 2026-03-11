9d34fdfe7b400904facc89abdf2a817eba6007bd
Israt Jahan Jisha <isratjahanfci@gmail.com>
2026-03-11 23:20:13 +0600

    create cloudinary file upload & delete utilitis

--- src/app/config/cloudinary.config.ts
+++ src/app/config/cloudinary.config.ts
@@ -1,10 +1,82 @@
-import { v2 as cloudinary } from "cloudinary";
+import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
 import { envVars } from "./env";
+import AppError from "../errorHelpers/AppError";
+import status from "http-status";
+
 
 cloudinary.config({
   cloud_name: envVars.CLOUDINARY_CLOUD_NAME,
   api_key: envVars.CLOUDINARY_API_KEY,
   api_secret: envVars.CLOUDINARY_API_SECRET,
 });
+
+
+export const uploadFileToCloudinary = async (buffer: Buffer , fileName : string): Promise<UploadApiResponse> =>{
+
+  if(!buffer && !fileName){
+    throw new AppError(status.BAD_REQUEST,'File not found')
+  }
+   
+      const extension = fileName.split(".").pop()?.toLocaleLowerCase();
+
+    const fileNameWithoutExtension = fileName
+        .split(".")
+        .slice(0, -1)
+        .join(".")
+        .toLowerCase()
+        .replace(/\s+/g, "-")
+        // eslint-disable-next-line no-useless-escape
+        .replace(/[^a-z0-9\-]/g, "");
+
+    const uniqueName =
+        Math.random().toString(36).substring(2) +
+        "-" +
+        Date.now() +
+        "-" +
+        fileNameWithoutExtension;
+
+    const folder = extension === "pdf" ? "pdfs" : "images";
+
+    return new Promise ((resolve , reject)=>{
+      cloudinary.uploader.upload_stream({
+        folder : `ph-healthcare/${folder}`,
+        public_id : uniqueName,
+        resource_type : "auto"
+      } , (error , result)=>{
+      if(error){
+        return reject(new AppError(status.INTERNAL_SERVER_ERROR , "Failed uploading file to cloudinary"))
+      }else{
+        resolve(result as UploadApiResponse)
+      }
+      })
+    })
+
+
+}
+
+export const deleteFileFromCloudinary = async (url : string) => {
+
+    try {
+        const regex = /\/v\d+\/(.+?)(?:\.[a-zA-Z0-9]+)+$/;
+
+        const match = url.match(regex);
+
+        if (match && match[1]) {
+            const publicId = match[1];
+
+            await cloudinary.uploader.destroy(
+                publicId, {
+                resource_type: "image"
+            }
+            )
+
+            console.log(`File ${publicId} deleted from cloudinary`);
+        }
+
+    } catch (error) {
+        console.error("Error deleting file from Cloudinary:", error);
+        throw new AppError(status.INTERNAL_SERVER_ERROR, "Failed to delete file from Cloudinary");
+    }
+}
 
 export const cloudinaryUpload = cloudinary; 
